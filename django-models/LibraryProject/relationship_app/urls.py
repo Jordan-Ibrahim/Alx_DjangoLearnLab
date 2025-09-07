0bd2b42cb8dd23273fd1dc1c0401bd5783fdab4b
Jordan Ibrahim <jordanibrahim97@gmail.com>
2025-09-07 21:34:36 +0100

    Task 1

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,9 +2,18 @@ from django.contrib import admin
 from django.urls import path
 from .views import list_books
 from . import views
+from .views import SignUpView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('books/', views.list_books, name='list-books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail'),
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,15 @@ from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
 
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
 def list_books(request):
       
       books = Book.objects.all()  
