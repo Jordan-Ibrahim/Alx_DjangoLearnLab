dff9d3045255d63f733ef8f0651c4e4722d4f040
Jordan Ibrahim <jordanibrahim97@gmail.com>
2025-09-14 22:52:23 +0100

    Task 0

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,27 @@
 
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from .models import CustomUserAdmin
+
+class ModelAdmin (admin.ModelAdmin):
+    list_display = ("username", "email", "role", "phone_number", "is_staff", "is_superuser")
+    list_filter = ("role", "is_staff", "is_superuser")
+
+    
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("role", "phone_number")}),
+    )
+
+  
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {"fields": ("role", "phone_number")}),
+    )
+
+    search_fields = ("username", "email", "role")
+    ordering = ("username",)
+
 
 class BookAdmin(admin.ModelAdmin):
    list_display = ('title', 'author', 'publication_year')
@@ -8,6 +29,6 @@ class BookAdmin(admin.ModelAdmin):
    list_filter = ('author', 'publication_year')
 
 
-admin.site.register(Book, BookAdmin)
+admin.site.register(Book, BookAdmin, CustomUser, CustomUserAdmin)
 
 
--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+from .models import CustomUserAdmin
 
 class ModelAdmin (admin.ModelAdmin):
     list_display = ("username", "email", "role", "phone_number", "is_staff", "is_superuser")
@@ -19,5 +20,5 @@ class ModelAdmin (admin.ModelAdmin):
     search_fields = ("username", "email", "role")
     ordering = ("username",)
 
-admin.site.register(CustomUser, ModelAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
 
