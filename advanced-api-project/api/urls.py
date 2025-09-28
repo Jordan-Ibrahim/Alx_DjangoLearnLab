9ac165d1d2b3a450db195136fc06d04da7b19d0f
Jordan Ibrahim <jordanibrahim97@gmail.com>
2025-09-28 22:16:54 +0100

    Task 1

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,9 +1,14 @@
-from django.contrib import admin
 from django.urls import path
-from .views import BookListCreateView, BookDetailView
-   
+from .views import (
+    BookListView,
+    BookCreateView,
+    BookUpdateView,
+    BookDeleteView,
+)
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('books/', BookListCreateView.as_view(), name='book-list-create'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/', BookListView.as_view(), name='book-list'),                  # GET all books
+    path('books/create/', BookCreateView.as_view(), name='book-create'),       # POST create
+    path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'), # PUT/PATCH update
+    path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'), # DELETE delete
 ]
