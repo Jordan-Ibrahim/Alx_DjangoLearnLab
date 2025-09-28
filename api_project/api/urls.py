f23cb1baabaa851ec3aca41a3cd2058d19753fc0
Jordan Ibrahim <jordanibrahim97@gmail.com>
2025-09-28 18:50:19 +0100

    Task 1

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from api.views import BookViewset
 from rest_framework import routers
+from .views import BookList
 
 # Router instance
 router = routers.DefaultRouter()
@@ -9,6 +10,7 @@ router.register(r'books', BookViewset, basename='book')  # simpler name
 
 urlpatterns = [
     
-    path('', include(router.urls)),  # includes all CRUD endpoints
+    path('', include(router.urls)),
+    path('books/', BookList.as_view(), name='book-list') # includes all CRUD endpoints
 ]
 
