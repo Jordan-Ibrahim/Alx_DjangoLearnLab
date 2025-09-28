47b8adaa3d08c723218c4d7554e679fb331aeb94
Jordan Ibrahim <jordanibrahim97@gmail.com>
2025-09-28 18:59:32 +0100

    Task 1

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -10,7 +10,7 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-class BookViewset(viewsets.ModelViewSet):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated] # Only authenticated users can access
