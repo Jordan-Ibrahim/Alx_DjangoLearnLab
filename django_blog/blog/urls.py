3950ba903f74c413a1dd8bb353cd3033d5acc69b
Jordan Ibrahim <jordanibrahim97@gmail.com>
2025-10-12 22:20:03 +0100

    Task

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path("post/", views.PostListView.as_view(), name='post_list'),
     path("post/new/", views.PostCreateView.as_view(), name='post_create'),
     path("post/<int:pk>/", views.PostDetailView.as_view(), name='post_detail'),
-    path("post/<int:pk>/edit/", views.PostUpdateView.as_view(), name='post_update'),
+    path("post/<int:pk>/update/", views.PostUpdateView.as_view(), name='post_update'),
     path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name='post_delete'),
     path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
