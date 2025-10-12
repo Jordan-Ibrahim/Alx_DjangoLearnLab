28ad8ca24e1e1c56eac2c0acdfc160c6c76b8c54
Jordan Ibrahim <jordanibrahim97@gmail.com>
2025-10-12 23:42:42 +0100

    Task

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -19,7 +19,7 @@ class PostForm(forms.ModelForm):
                 'placeholder': 'Write your post content here...',
                 'rows': 8,
             }),
-            'tags': TagWidget(attrs={'class': 'form-control'}),
+            'tags': TagWidget(),
         }
         
         labels = {
