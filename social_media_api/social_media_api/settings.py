84237c0c56a98c93aebd9c7d141f7ce13ee3124b
Jordan Ibrahim <jordanibrahim97@gmail.com>
2025-10-18 23:03:18 +0100

    Task 4

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,3 +142,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"   # for collectstatic
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+PORT = os.environ.get("PORT", "8000")
